4c89bd7e48fc0c0fd9d4f1ba04ea1fd21b04a1cb
Christian Plessl <christian@plesslweb.ch>
2009-08-12 21:40:03 +0200

    Refactored printing of fields

--- render-bibliography.rb
+++ render-bibliography.rb
@@ -21,11 +21,8 @@ end
 
 
 def text_for_field(fieldname, p, params)
-  prefix = params[:prefix]
-  postfix = params[:postfix]
-  
   if field(p,fieldname) then
-    return prefix + field(p,fieldname) + postfix
+    return (params[:prefix] || "") + field(p,fieldname) + (params[:postfix] || "")
   else
     return ""
   end
@@ -41,65 +38,19 @@ def render_inproceedings(p)
     r += p.editors.map {|e| e.abbreviated_name}.joined_by_comma_and_and + ", editors, "
   end
 
-  r += field(p,"Booktitle").detex + ", "
-
-  if field(p,"Volume") then
-    r += ", volume " + field(p,"Volume") + " "
-  end
-
-  if field(p,"Number") then
-    r += ", number " + field(p,"Number") + " "
-  end
-
-  if field(p,"Series") then
-    r += "of " + field(p,"Series").detex + ", "
-  end
-
-  r += text_for_field("Pages", p, :prefix => ", pages ", :postfix => ". ").detex
-
-#  if field(p,"Pages") then
-#    r += ", pages " + field(p,"Pages").detex + ". "
-#  end
-
-  if field(p,"Address") then
-    r += field(p,"Address").detex + ", "
-  end
-
-  if field(p,"Organization") then
-    r += field(p,"Organization").detex + ", "
-  end
-
-  if field(p,"Month") then
-    r += field(p,"Month").detex + " "
-  end
-
-  if field(p,"Year") then
-    r += field(p,"Year").detex + ". "
-  end
-
-  if field(p,"Publisher") then
-    r += field(p,"Publisher").detex + ". "
-  end
-
-  if field(p,"Note") then
-    r += field(p,"Note").detex + ". "
-  end
+  r += text_for_field("Booktitle", p, :postfix => ", ").detex
+  r += text_for_field("Volume", p, :prefix => "volume ", :postfix => " ")
+  r += text_for_field("Number", p, :prefix => "number ", :postfix => " ")
+  r += text_for_field("Series", p, :prefix => "of ", :postfix => ", ")
+  r += text_for_field("Pages", p, :prefix => "pages ", :postfix => ". ").detex
+  r += text_for_field("Address", p, :postfix => ", ").detex
+  r += text_for_field("Organization", p, :postfix => ", ").detex
+  r += text_for_field("Publisher", p, :postfix => ". ").detex
+  r += text_for_field("Month", p, :postfix => " ").detex
+  r += text_for_field("Year", p, :postfix => ". ").detex
+  r += text_for_field("Note", p, :postfix => ". ")
   
   return r
 end
-
-  
-#  <?$pubType=inproceedings?>
-#  	<$authors.abbreviatedName.@componentsJoinedByCommaAndAnd/>. <$fields.Title.titleCapitalizedString.stringByRemovingTeX/>. In #<$editors?><$editors.abbreviatedName.@componentsJoinedByCommaAndAnd/>, editors, </$editors?><$fields.Booktitle.stringByRemovingTeX/>
-#<$fields.Volume?>, volume <$fields.Volume/>
-#<$fields.Series?> of <$fields.Series/></$fields.Series?>
-#<?$fields.Volume?><$fields.Number?>, number <$fields.Number/><$fields.Series?> in <$fields.Series/></$fields.Series?><?$fields.Number?><$fields.Series.stringByPrependingCommaAndSpaceIfNotEmpty/></$fields.Number?></$fields.Volume?>
-#<$fields.Pages.stringByPrependingCommaAndSpaceIfNotEmpty/>
-#<$fields.Address?>, <$fields.Address/>,
-# <$fields.Month.stringByAppendingSpaceIfNotEmpty/><$fields.Year/>.
-# <$fields.Organization/>, 
-#<$fields.Publisher/>
-#<?$fields.Address?>, <$fields.Organization/>, <$fields.Publisher/>, #<$fields.Month.stringByAppendingSpaceIfNotEmpty/><$fields.Year/></$fields.Address?>
-#<$fields.Note.stringByPrependingFullStopAndSpace/>.
 
 
